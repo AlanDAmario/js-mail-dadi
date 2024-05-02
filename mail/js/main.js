a4cdf8cc47481781f1d9298b4d0fdab8331c6a7d
AlanDAmario <vega93@live.it>
2024-05-02 16:33:45 +0200

    [upd] mail end

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,12 +1,22 @@
 'use strict';
 //TRAMITE PROMPT CHIEDIAMO L EMAIL ALL UTENTE
 const userMail = prompt('inserisci la tua mail');
+console.log();
 const validMail = ['ciccio@gmail.com', 'bambi@gmail.com', 'topogigio@gmail.com', 'sbrodolino@gmail.com', 'cortana@gmail.com'];
-console.log(validMail);
-
-if (userMail === validMail) {
+// console.log(validMail);
+//DICHIARIAMO UN VARIABILE PER VALIDARE LA MAIL E STAMPARLA UNA SINGOLA VOLTA//
+let acessDenied = true;
+// ESECUZIONE DEL LOOP//
+for (let i = 0; i < validMail.length; i++) {
+    console.log(validMail[i]);
+    if (userMail === validMail[i]) {
+        acessDenied = false
+    }
+}
+console.log(acessDenied);
+//VALIDAZIONE EMAIL
+if (acessDenied !== true) {
     console.log('hai l accesso');
-} else if (userMail !== validMail) {
-    console.log('accesso negato')
-
+} else {
+    console.log('accesso negato');
 }
