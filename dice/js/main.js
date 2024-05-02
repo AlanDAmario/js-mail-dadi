5b6b4f636a1eb55f1e3b5147873991fb7b4df145
AlanDAmario <vega93@live.it>
2024-05-02 17:06:02 +0200

    [UPD] dice end

--- dice/js/main.js
+++ dice/js/main.js
@@ -1,1 +1,25 @@
 'use strict';
+//GENEREZIONE RANDOMICA NUMERO PER UTENTE E PC METODO CLASSICO//
+// const pcNumber = Math.floor(Math.random() * 6 + 1);
+// console.log(pcNumber, 'scelta del pc');
+// const userNumber = Math.floor(Math.random() * 6 + 1);
+// console.log(userNumber, 'scelta utente');
+
+
+//GENERAZIONE RANDOMICA TRAMITE ARRAY//
+const pcNumber = [1, 2, 3, 4, 5, 6];
+const varPcNumber = pcNumber[Math.floor(pcNumber.length * Math.random())];
+console.log(varPcNumber, 'numero del pc');
+const userNumber = [1, 2, 3, 4, 5, 6];
+const varUserNumber = userNumber[Math.floor(userNumber.length * Math.random())];
+console.log(varUserNumber, 'numero del giocatore');
+
+
+// VINCITORE//
+if (varPcNumber > varUserNumber) {
+    console.log('il pc vince');
+} else if (varPcNumber < varUserNumber) {
+    console.log('il giocatore vince');
+} else {
+    console.log('pareggio');
+}
