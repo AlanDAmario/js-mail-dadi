86b7e9c6195ff0cbd4fddb46474e48c2a27771cb
AlanDAmario <vega93@live.it>
2024-05-02 15:43:22 +0200

    [UPD] check

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ stampa un messaggio appropriato sull’esito del controllo.
 
 
 
+
 Gioco dei dadi
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
--- mail/js/main.js
+++ mail/js/main.js
@@ -1,1 +1,12 @@
 'use strict';
+//TRAMITE PROMPT CHIEDIAMO L EMAIL ALL UTENTE
+const userMail = prompt('inserisci la tua mail');
+const validMail = ['ciccio@gmail.com', 'bambi@gmail.com', 'topogigio@gmail.com', 'sbrodolino@gmail.com', 'cortana@gmail.com'];
+console.log(validMail);
+
+if (userMail === validMail) {
+    console.log('hai l accesso');
+} else if (userMail !== validMail) {
+    console.log('accesso negato')
+
+}
